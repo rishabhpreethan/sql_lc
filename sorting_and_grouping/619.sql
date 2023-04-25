8f9084992fcd412dfa8c321e57b88e5b04194746
rishabhpreethan <rishabh.preethan@gmail.com>
2023-04-25 19:05:11 +0530

    day 3

---
+++ sorting_and_grouping/619.sql
@@ -0,0 +1,58 @@
+-- A single number is a number that appeared only once in the MyNumbers table.
+-- Write an SQL query to report the largest single number. If there is no single number, report null.
+-- The query result format is in the following example.
+
+
+-- Example 1:
+
+-- Input:
+
+-- MyNumbers table:
+
+-- | num |
+-- +-----+
+-- | 8   |
+-- | 8   |
+-- | 3   |
+-- | 3   |
+-- | 1   |
+-- | 4   |
+-- | 5   |
+-- | 6   |
+
+-- Output: 
+-- | num |
+-- +-----+
+-- | 6   |
+
+-- Explanation: The single numbers are 1, 4, 5, and 6.
+-- Since 6 is the largest single number, we return it.
+
+-- Example 2:
+
+-- Input:
+
+-- MyNumbers table:
+-- | num |
+-- +-----+
+-- | 8   |
+-- | 8   |
+-- | 7   |
+-- | 7   |
+-- | 3   |
+-- | 3   |
+-- | 3   |
+
+-- Output: 
+-- +------+
+-- | num  |
+-- +------+
+-- | null |
+
+-- Explanation: There are no single numbers in the input table so we return null.
+
+
+
+
+select max(a.num) as num
+from (select num from mynumbers group by num having count(*)=1) as a;
